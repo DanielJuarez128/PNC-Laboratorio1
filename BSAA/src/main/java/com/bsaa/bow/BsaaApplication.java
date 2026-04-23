f9c940bbb82525dc4865e55d5bf8a8bfe597836d
DanielJuarez <00012922@uca.edu.sv>
2026-04-23 16:20:30 -0600

    Init project

---
+++ BSAA/src/main/java/com/bsaa/bow/BsaaApplication.java
@@ -0,0 +1,13 @@
+package com.bsaa.bow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsaaApplication.class, args);
+    }
+
+}
---
+++ BSAA/src/test/java/com/bsaa/bow/BsaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsaa.bow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
